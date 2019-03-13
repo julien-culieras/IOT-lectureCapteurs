70a79a1aed87362bfc6aadecfde393a25c04ed94
Julien CULIERAS <julien.culieras@gmail.com>
2019-03-13 11:41:46 +0100

    migrations

---
+++ database/migrations/2019_03_13_103659_create_records_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('value');
+
+            $table->integer('sensor_id')->unsigned();
+            $table->foreign('sensor_id')
+                ->references('id')
+                ->on('sensors')
+                ->onDelete('CASCADE');
+
+            $table->timestamp('recorded_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
